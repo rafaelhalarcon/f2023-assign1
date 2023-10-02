f810f7b34ab8e41e045d7c515882815320670deb
Rafael Hernandez Alarcon <80131620+rafaelhalarcon@users.noreply.github.com>
2023-10-02 09:32:35 -0600

    Initial structure and metatags

---
+++ www/page1.php
@@ -0,0 +1,21 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Arthur Bakir and Rafael Hernandez Alarcon" />
+    <meta name="keywords" content="Music BPM Energy danceability acoustic" />
+    <meta name="description" content="WEBII assignment 1 music browser" />
+    <link rel="stylesheet" href="../style/reset.css">
+    <link rel="icon" type="image/x-icon" href="">
+    <title>Music browser</title>
+</head>
+</head>
+<body>
+    
+</body>
+</html>
