36cf2a075f774275ce31ee304cbcf6c1e555c5c9
Rafael Hernandez Alarcon <80131620+rafaelhalarcon@users.noreply.github.com>
2023-10-02 21:16:54 -0600

    Included MaterialDesign CDN links and footer

--- www/index.php
+++ www/index.php
@@ -1,20 +1,30 @@
-<?php include('./configdb.inc.php'); ?>
+<?php include('./configdb.inc.php');
+include('./phpcomponents.inc.php') ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<head>
-    <meta charset="UTF-8">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="author" content="Arthur Bakir and Rafael Hernandez Alarcon" />
-    <meta name="keywords" content="Music BPM Energy danceability acoustic" />
-    <meta name="description" content="WEBII assignment 1 music browser" />
-    <link rel="stylesheet" href="../style/reset.css">
-    <link rel="icon" type="image/x-icon" href="">
-    <title>Music browser</title>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Arthur Bakir and Rafael Hernandez Alarcon" />
+        <meta name="keywords" content="Music BPM Energy danceability acoustic" />
+        <meta name="description" content="WEBII assignment 1 music browser" />
+        <link rel="stylesheet" href="../style/reset.css">
+        <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+        <!-- MDB -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="">
+        <title>Music browser</title>
+    </head>
 </head>
-</head>
+
 <body>
     <h1>Is this working</h1>
 
@@ -35,4 +45,8 @@
     }
     ?>
 </body>
+<footer>
+    <?=footer(); ?>
+</footer>
+
 </html>
